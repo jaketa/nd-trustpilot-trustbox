efeba5004a583bc53e0e3145d41fb48cb71fa7ca
Jake <jake.t.ashley@gmail.com>
2021-07-15 21:16:28 -0400

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ class Application extends React.Component {
     return (
         <div className="application">
             <Helmet>
-              <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+              <script type="text/javascript" src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
             </Helmet>
             <div class="trustpilot-widget" data-locale="en-US" data-template-id="539ad60defb9600b94d7df2c" data-businessunit-id="571fa0520000ff00058c0de4" data-style-height="500px" data-style-width="100%" data-theme="light" data-stars="1,2,3,4,5" data-review-languages="en">
               <a href="https://www.trustpilot.com/review/newdayusa.com" target="_blank" rel="noopener">Trustpilot</a>
