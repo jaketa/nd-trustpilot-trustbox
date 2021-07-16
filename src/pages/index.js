1ffebfce309ee7767f241be5f4407b6913f31c52
Jake <jake.t.ashley@gmail.com>
2021-07-15 20:57:26 -0400

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-
-function trustPilot() {
+import React from "react";
+import {Helmet} from "react-helmet";
+ 
+class Application extends React.Component {
+  render () {
     return (
-      <div className="trustPilot">
-        <Helmet>
-          <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-        </Helmet>
-        <div class="trustpilot-widget" data-locale="en-US" data-template-id="539ad60defb9600b94d7df2c" data-businessunit-id="571fa0520000ff00058c0de4" data-style-height="500px" data-style-width="100%" data-theme="light" data-stars="1,2,3,4,5" data-review-languages="en">
-          <a href="https://www.trustpilot.com/review/newdayusa.com" target="_blank" rel="noopener">Trustpilot</a>
+        <div className="application">
+            <Helmet>
+              <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+            </Helmet>
+            <div class="trustpilot-widget" data-locale="en-US" data-template-id="539ad60defb9600b94d7df2c" data-businessunit-id="571fa0520000ff00058c0de4" data-style-height="500px" data-style-width="100%" data-theme="light" data-stars="1,2,3,4,5" data-review-languages="en">
+              <a href="https://www.trustpilot.com/review/newdayusa.com" target="_blank" rel="noreferrer">Trustpilot</a>
+            </div>
         </div>
-      </div>
     );
   }
+};
 
-  export default trustPilot;
+export default Application
